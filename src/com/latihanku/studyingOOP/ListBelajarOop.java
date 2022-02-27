4865f8f3102d6a052eb64c5b0131514fdf333ba5
Roby <robykurniaaa@gmail.com>
2022-02-27 19:31:57 +0700

    menambah package belajar oop di file latihanku dan menambah class list belajar oop

---
+++ src/com/latihanku/studyingOOP/ListBelajarOop.java
@@ -0,0 +1,11 @@
+package com.latihanku.studyingOOP;
+
+public class ListBelajarOop {
+    public static void main(String[] args) {
+        System.out.println("List Belajar OOP");
+        System.out.println("1. Belajar Method");
+        System.out.println("2. Belajar Class");
+        System.out.println("3. Belajar Inheritance");
+        System.out.println("next soon");
+    }
+}
