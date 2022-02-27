54112de137882f5e22e361b61236fffb586d2f36
Roby <robykurniaaa@gmail.com>
2022-02-27 19:34:08 +0700

    menambah dua poin list belajar oop

--- src/com/latihanku/studyingOOP/ListBelajarOop.java
+++ src/com/latihanku/studyingOOP/ListBelajarOop.java
@@ -6,6 +6,8 @@ public class ListBelajarOop {
         System.out.println("1. Belajar Method");
         System.out.println("2. Belajar Class");
         System.out.println("3. Belajar Inheritance");
+        System.out.println("4. Belajar Pewarisan");
+        System.out.println("5. Menamatkan tutorial java oop di youtube programmer zaman now");
         System.out.println("next soon");
     }
 }
