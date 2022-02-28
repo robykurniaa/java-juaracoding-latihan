cb16c72f7d027493f291e028b90e98df1e0d06af
Roby <robykurniaaa@gmail.com>
2022-02-28 17:29:47 +0700

    menambah beberapa file

---
+++ src/com/juaracoding/array/Main.java
@@ -0,0 +1,19 @@
+package com.juaracoding.array;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] kopi = new String[3];
+        
+        for (var i=0;i< kopi.length;i++){
+            System.out.print("Kopi ke - "+ (i+1) +" :");
+            kopi[i] = scan.nextLine();
+        }
+        System.out.println("=========================================");
+        for (String i :kopi) {
+            System.out.println(i);
+        }
+        }
+    }
--- src/com/juaracoding/basicjava/Polindrome.java
+++ src/com/juaracoding/basicjava/Polindrome.java
@@ -2,12 +2,17 @@ package com.juaracoding.basicjava;
 
 public class Polindrome {
     public static void main(String[] args) {
-        String cekPolindrome = "Katak";
-        StringBuilder build = new StringBuilder(cekPolindrome);
-        build.reverse();
-        String polindrome = build.toString();
-
-        System.out.println(polindrome);
+    String cekPolindrome= "";
+    StringBuilder build = new StringBuilder(cekPolindrome);
+    build.reverse();
+    String cek = build.toString();
+        System.out.println("Original Text : " + cekPolindrome);
+        System.out.println("Cek Polindrome : " + cek);
+        if (cekPolindrome.equalsIgnoreCase(cek)) {
+            System.out.println("Polindrome");
+        }else{
+            System.out.println("Not Polindrome");
+        }
 
     }
 }
